18dc4041d5ada6ae7fb4a9b4417fd51dee987fc3
uni_kakurenbo <kohma1116@ybb.ne.jp>
2025-06-21 10:51:39 +0900

    client: add color pallets

--- client/src/plugins/vuetify.ts
+++ client/src/plugins/vuetify.ts
@@ -14,6 +14,21 @@ import 'vuetify/styles'
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
-    defaultTheme: 'dark',
+    themes: {
+      light: {
+        dark: false,
+        colors: {
+          primary: '#FFC300',
+          secondary: '#B2BDC7',
+          warning: '#F26451',
+
+          primaryText: '#EFF0F0',
+          secondaryText: '#333E47',
+          text: '#222425',
+
+          background: '#FFFFFF',
+        },
+      },
+    },
   },
 })
