3bf1775dce2e10a145db686951641ce91c5ff5f8
howard127 <60410935+howard12721@users.noreply.github.com>
2025-06-21 15:13:03 +0900

    review_repositoryのインターフェースを変更

--- server/internal/domain/repository/review_repository.go
+++ server/internal/domain/repository/review_repository.go
@@ -9,7 +9,12 @@ import (
 type ReviewRepository interface {
 	Save(ctx context.Context, user *model.Review) error
 	FindByID(ctx context.Context, id uuid.UUID) (*model.Review, error)
-	FindByShopID(ctx context.Context, shopID uuid.UUID) ([]*model.Review, error)
-	FindRecentReviews(ctx context.Context, offset int, limit int) ([]*model.Review, error)
+	FindRecentReviews(
+		ctx context.Context,
+		limit int,
+		offset int,
+		shopID uuid.UUID,
+		authorID uuid.UUID,
+	) ([]*model.Review, error)
 	Delete(ctx context.Context, id uuid.UUID) error
 }
