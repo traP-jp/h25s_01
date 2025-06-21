3d09cde266b8ec9d0943eaf21b030e00a7f7e55b
しょーご <136148494+sh0go07@users.noreply.github.com>
2025-06-22 01:48:32 +0900

    lint が落ちてたから直した

--- client/src/components/Shops/ShopsList.vue
+++ client/src/components/Shops/ShopsList.vue
@@ -6,57 +6,59 @@
     @load="load"
   >
     <template
-      v-for="item in items"
-      :key="item"
-      style="
-        width: 100%;
-        max-width: 351px;
-        border-radius: 6px;
-        overflow: hidden;
-        margin: 0 auto 16px auto;
-        box-shadow: 0 2px 8px rgba(0,0,0,0.04);
-      "
+      v-for="item in items" :key="item"
     >
       <div
         style="
-          display: grid;
-          grid-template-columns: 2fr 1fr;
-          grid-template-rows: 1fr 1fr;
-          height: 170px;
-          gap: 2px;
+          width: 100%;
+          max-width: 351px;
+          border-radius: 6px;
+          overflow: hidden;
+          margin: 0 auto 16px auto;
+          box-shadow: 0 2px 8px rgba(0,0,0,0.04);
         "
       >
-        <v-img
-          style="grid-row: 1 / 3; grid-column: 1 / 2; object-fit: cover;"
-          src="https://cdn.vuetifyjs.com/images/cards/house.jpg"
-          width="100%"
-          height="100%"
-          cover
-        ></v-img>
-        <v-img
-          style="grid-row: 1 / 2; grid-column: 2 / 3; object-fit: cover;"
-          src="https://cdn.vuetifyjs.com/images/cards/house.jpg"
-          width="100%"
-          height="100%"
-          cover
-        ></v-img>
-        <v-img
-          style="grid-row: 2 / 3; grid-column: 2 / 3; object-fit: cover;"
-          src="https://cdn.vuetifyjs.com/images/cards/house.jpg"
-          width="100%"
-          height="100%"
-          cover
-        ></v-img>
-      </div>
+        <div
+          style="
+            display: grid;
+            grid-template-columns: 2fr 1fr;
+            grid-template-rows: 1fr 1fr;
+            height: 170px;
+            gap: 2px;
+          "
+        >
+          <v-img
+            style="grid-row: 1 / 3; grid-column: 1 / 2; object-fit: cover;"
+            src="https://cdn.vuetifyjs.com/images/cards/house.jpg"
+            width="100%"
+            height="100%"
+            cover
+          ></v-img>
+          <v-img
+            style="grid-row: 1 / 2; grid-column: 2 / 3; object-fit: cover;"
+            src="https://cdn.vuetifyjs.com/images/cards/house.jpg"
+            width="100%"
+            height="100%"
+            cover
+          ></v-img>
+          <v-img
+            style="grid-row: 2 / 3; grid-column: 2 / 3; object-fit: cover;"
+            src="https://cdn.vuetifyjs.com/images/cards/house.jpg"
+            width="100%"
+            height="100%"
+            cover
+          ></v-img>
+        </div>
 
-      <v-card class="pl-0">
-        <v-card-title>
-          <p class="color text-text text-caption left">お店の名前</p>
-        </v-card-title>
-        <v-card-subtitle>
-          <p class="color text-secondary text-caption left">○○駅周辺 / text text / text text</p>
-        </v-card-subtitle>
-      </v-card>
+        <v-card class="pl-0">
+          <v-card-title>
+            <p class="color text-text text-caption left">お店の名前</p>
+          </v-card-title>
+          <v-card-subtitle>
+            <p class="color text-secondary text-caption left">○○駅周辺 / text text / text text</p>
+          </v-card-subtitle>
+        </v-card>
+      </div>
     </template>
   </v-infinite-scroll>
 </template>
