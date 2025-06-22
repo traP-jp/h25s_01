03dd80f098f5047b92a0ecc714f9a76952f6df08
sh0go07 <shogotitech@gmail.com>
2025-06-22 10:54:11 +0900

    client: shops の api を持ってきた

--- client/src/pages/Shops.vue
+++ client/src/pages/Shops.vue
@@ -1,8 +1,24 @@
 <template>
   <div id="container">
-    <ShopList id="shop-list" class="mx-auto" />
+    <ShopList id="shop-list" class="mx-auto" :shops="shops" />
   </div>
 </template>
+
+<script setup>
+  import { onMounted, ref } from 'vue'
+
+  const shops = ref([])
+
+  onMounted(async () => {
+    try {
+      const res = await fetch('/api/v1/shops')
+      if (!res.ok) throw new Error('Failed to fetch shops')
+      shops.value = await res.json()
+    } catch (error) {
+      console.error(error)
+    }
+  })
+</script>
 
 <style scoped>
   #container {
